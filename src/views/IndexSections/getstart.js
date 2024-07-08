98fbac50b0ab7b6a6b637a358ff126192829d443
EBD <44670838+EBD476@users.noreply.github.com>
2024-07-08 14:13:30 +0330

    first commit

---
+++ src/views/IndexSections/getstart.js
@@ -0,0 +1,244 @@
+/*!
+
+=========================================================
+* BLK Design System React - v1.2.2
+=========================================================
+
+* Product Page: https://www.creative-tim.com/product/blk-design-system-react
+* Copyright 2023 Creative Tim (https://www.creative-tim.com)
+* Licensed under MIT (https://github.com/creativetimofficial/blk-design-system-react/blob/main/LICENSE.md)
+
+* Coded by Creative Tim
+
+=========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+*/
+import React from "react";
+import classnames from "classnames";
+// plugin that creates slider
+// import Slider from "nouislider";
+
+// reactstrap components
+import {
+  Button,
+  Label,
+  FormGroup,
+  CustomInput,
+  Input,
+  InputGroupAddon,
+  InputGroupText,
+  InputGroup,
+  Container,
+  Row,
+  Col,
+  Card,
+  CardHeader,
+  CardBody,
+  CardFooter,
+  CardTitle,
+  UncontrolledTooltip
+} from "reactstrap";
+
+export default function Basics() {
+  const [inputFocus, setInputFocus] = React.useState(false);
+  // const slider1 = React.useRef(null);
+  // const slider2 = React.useRef(null);
+  // React.useEffect(() => {
+  //   Slider.create(slider1.current, {
+  //     start: [40],
+  //     connect: [true, false],
+  //     step: 1,
+  //     range: { min: 0, max: 100 },
+  //   });
+  //   Slider.create(slider2.current, {
+  //     start: [20, 60],
+  //     connect: [false, true, false],
+  //     step: 1,
+  //     range: { min: 0, max: 100 },
+  //   });
+  // }, []);
+  return (
+    <div className="section section-basic" id="basic-elements">
+             <section className="section section-lg">
+          <section className="section">
+            <img
+              alt="..."
+              className="path"
+              src={require("assets/img/path4.png")}
+            />
+            <Container>
+              <Row className="row-grid justify-content-between">
+                <Col className="mt-lg-5" md="5">
+                  <Row>
+                    <Col className="px-2 py-2" lg="6" sm="12">
+                      <Card className="card-stats">
+                        <CardBody>
+                          <Row>
+                            <Col md="4" xs="5">
+                              <div className="icon-big text-center icon-warning">
+                                <i className="tim-icons icon-trophy text-warning" />
+                              </div>
+                            </Col>
+                            <Col md="8" xs="7">
+                              <div className="numbers">
+                                <CardTitle tag="p">3,237</CardTitle>
+                                <p />
+                                <p className="card-category">Awards</p>
+                              </div>
+                            </Col>
+                          </Row>
+                        </CardBody>
+                      </Card>
+                    </Col>
+                    <Col className="px-2 py-2" lg="6" sm="12">
+                      <Card className="card-stats upper bg-default">
+                        <CardBody>
+                          <Row>
+                            <Col md="4" xs="5">
+                              <div className="icon-big text-center icon-warning">
+                                <i className="tim-icons icon-coins text-white" />
+                              </div>
+                            </Col>
+                            <Col md="8" xs="7">
+                              <div className="numbers">
+                                <CardTitle tag="p">3,653</CardTitle>
+                                <p />
+                                <p className="card-category">Commits</p>
+                              </div>
+                            </Col>
+                          </Row>
+                        </CardBody>
+                      </Card>
+                    </Col>
+                  </Row>
+                  <Row>
+                    <Col className="px-2 py-2" lg="6" sm="12">
+                      <Card className="card-stats">
+                        <CardBody>
+                          <Row>
+                            <Col md="4" xs="5">
+                              <div className="icon-big text-center icon-warning">
+                                <i className="tim-icons icon-gift-2 text-info" />
+                              </div>
+                            </Col>
+                            <Col md="8" xs="7">
+                              <div className="numbers">
+                                <CardTitle tag="p">593</CardTitle>
+                                <p />
+                                <p className="card-category">Presents</p>
+                              </div>
+                            </Col>
+                          </Row>
+                        </CardBody>
+                      </Card>
+                    </Col>
+                    <Col className="px-2 py-2" lg="6" sm="12">
+                      <Card className="card-stats">
+                        <CardBody>
+                          <Row>
+                            <Col md="4" xs="5">
+                              <div className="icon-big text-center icon-warning">
+                                <i className="tim-icons icon-credit-card text-success" />
+                              </div>
+                            </Col>
+                            <Col md="8" xs="7">
+                              <div className="numbers">
+                                <CardTitle tag="p">10,783</CardTitle>
+                                <p />
+                                <p className="card-category">Forks</p>
+                              </div>
+                            </Col>
+                          </Row>
+                        </CardBody>
+                      </Card>
+                    </Col>
+                  </Row>
+                </Col>
+                <Col md="6">
+                  <div className="pl-md-5">
+                    <h1>
+                      Large <br />
+                      Achivements
+                    </h1>
+                    <p>
+                      I should be capable of drawing a single stroke at the
+                      present moment; and yet I feel that I never was a greater
+                      artist than now.
+                    </p>
+                    <br />
+                    <p>
+                      When, while the lovely valley teems with vapour around me,
+                      and the meridian sun strikes the upper surface of the
+                      impenetrable foliage of my trees, and but a few stray.
+                    </p>
+                    <br />
+                    <a
+                      className="font-weight-bold text-info mt-5"
+                      href="#pablo"
+                      onClick={(e) => e.preventDefault()}
+                    >
+                      Show all{" "}
+                      <i className="tim-icons icon-minimal-right text-info" />
+                    </a>
+                  </div>
+                </Col>
+              </Row>
+            </Container>
+          </section>
+        </section>
+      <img alt="..." className="path" src={require("assets/img/path1.png")} />
+      <Container className="align-items-center">
+            <Row>
+              <Col lg="6" md="6">
+                <h1 className="profile-title text-left">Mike Scheinder</h1>
+                <h5 className="text-on-back">01</h5>
+                <p className="profile-description">
+                  Offices parties lasting outward nothing age few resolve.
+                  Impression to discretion understood to we interested he
+                  excellence. Him remarkably use projection collecting. Going
+                  about eat forty world has round miles.
+                </p>
+                <div className="btn-wrapper profile pt-3">
+                  <Button
+                    className="btn-icon btn-round"
+                    color="twitter"
+                    href="https://twitter.com/creativetim"
+                    id="tooltip639225725"
+                    target="_blank"
+                  >
+                    <i className="fab fa-twitter" />
+                  </Button>
+                  <UncontrolledTooltip delay={0} target="tooltip639225725">
+                    Follow us
+                  </UncontrolledTooltip>
+                  <Button
+                    className="btn-icon btn-round"
+                    color="facebook"
+                    href="https://www.facebook.com/creativetim"
+                    id="tooltip982846143"
+                    target="_blank"
+                  >
+                    <i className="fab fa-facebook-square" />
+                  </Button>
+                  <UncontrolledTooltip delay={0} target="tooltip982846143">
+                    Like us
+                  </UncontrolledTooltip>
+                  <Button
+                    className="btn-icon btn-round"
+                    color="dribbble"
+                    href="https://dribbble.com/creativetim"
+                    id="tooltip951161185"
+                    target="_blank"
+                  >
+                    <i className="fab fa-dribbble" />
+                  </Button>
+                </div>
+              </Col>
+           
+            </Row>
+          </Container>
+    </div>
+  );
+}
